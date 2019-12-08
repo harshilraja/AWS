2e48c78ec492163de5a3bb718dda4b5b469ba52c
Siva Prasad Raja <arpithareddy1985@gmail.com>
2019-12-08 20:35:16 +0400

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,4 @@
+export const config = {
+    hubName: 'testtutorialhub',
+    hubConnectionString: 'Endpoint=sb://testhubsiva.servicebus.windows.net/;SharedAccessKeyName=DefaultFullSharedAccessSignature;SharedAccessKey=l4K+ioPREJwwKz8qbw1WDE5l8ISzkTpFCIkU+h1TPZU='
+};
